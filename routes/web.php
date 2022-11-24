c50d3569d34a01b4bde742b8b7c53a444ca6a198
WelkerAcero <universityestudiosca@gmail.com>
2022-11-23 22:21:04 -0500

    UserFactory arreglado para usar

--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-
 Route::controller(SessionController::class)->group(function () {
     //These routes have authenticate verification on Controller
     Route::get('/preloader', 'loader')->name('preloader');
     Route::get('/login', 'index')->name('login');
     Route::post('/validating', 'auth')->name('login.validate');
     Route::get('signup', 'register')->name('register.form');
-    Route::get('/logout', 'logout')->name('logout');
+    Route::post('/logout', 'logout')->name('logout');
 });
 
 Route::group(['middleware' => ['auth']], function () {
